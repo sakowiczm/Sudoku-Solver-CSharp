a8e703fdbe42d7ff6b8c180ac2096b01599e7414
MS <sakowiczm@gmail.com>
2011-11-30 00:43:08 +0000

    change in approach

--- SudokuSolver/SudokuSolver.cs
+++ SudokuSolver/SudokuSolver.cs
@@ -5,6 +5,10 @@ using System.Text;
 
 namespace SudokuSolver
 {
+    // todo: try to use TPL
+    // todo: try to fit here strategy pattern
+    // todo: heavy on interfaces and unit tests
+
     public enum Block
     {
         B00,
@@ -20,11 +24,18 @@ namespace SudokuSolver
 
     public class Cell
     {
+        public Cell()
+        {
+            PossibleValues = new List<int>(9);
+        }
+
         public int X { get; set; }
         public int Y { get; set; }
 
         public int? Value { get; set; }
         public bool Initial { get; set; }
+
+        public List<int> PossibleValues { get; set; }
 
         public Block Block
         {
@@ -40,7 +51,9 @@ namespace SudokuSolver
     public class SudokuSolver
     {
         public Cell[,] _sudoku = new Cell[8,8];
+
         public List<Cell> _empty;
+        public List<Cell> _filled;
 
         // lista pol pustych - algorytm dziala tak dlugo az ta lista jest pusta - jesli po kolejnej iteracji - lista pol pustych sie nie zmniejszyla, znaczy ze pozostale elementy 'trzeba' zgadywac
         // lista pol wypelnionych -
@@ -49,6 +62,109 @@ namespace SudokuSolver
         {
             _sudoku = sudoku;
             _empty = GetEmptyCells(_sudoku);
+        }
+
+        public void SolveCurrent()
+        {
+            var emptyCells = GetEmptyCells(_sudoku);
+
+            int initialEmptyCellCount = emptyCells.Count;
+            bool proceed = true;
+
+            while (proceed)
+            {
+                foreach (var block in Block) // todo: for each block
+                {
+                    var cellsInBlock = emptyCells.Where(c => c.Block == block).ToList();
+
+                    int count = cellsInBlock.Count();
+
+                    if(count == 0)
+                        continue;
+
+                    if (count == 0)
+                    {
+                        // just fill this single value
+                    }
+
+                    if (count > 0)
+                    {
+                        if (count == 1)
+                        {
+                            // just fill this single value
+                        }
+                        else
+                        {
+                            var knownValues = _filled.Where(c => c.Block == block).Select(c => c.Value);
+                            List<int> notKnownValues = null; // todo: create based on knowValues;
+
+                            // build list of possible values
+                            foreach (var cell in cellsInBlock)
+                            {
+                                foreach (var value in notKnownValues)
+                                {
+                                    bool isPossible = IsPossible(cell, value);
+
+                                    if (isPossible)
+                                        cell.PossibleValues.Add(value);
+                                }
+                            }
+
+                            // ponizszy foreach zamienic na while  i tak samo untill notKnowValues nie sa = 0 badz wartosc jest taka sama jak na poczatku
+
+                            // if we have only on occurence of possible values for a cell fill the value
+                            foreach (var value in notKnownValues)
+                            {
+                                // reduce vertically
+                                if (cellsInBlock.Count(c => c.PossibleValues.Contains(value)) == 1)
+                                {
+                                    var cell = cellsInBlock.First(c => c.PossibleValues.Contains(value));
+
+                                    cell.Value = value;
+                                    cell.PossibleValues.Clear();
+                                    notKnownValues.Remove(value);
+                                    cellsInBlock.ForEach(c => c.PossibleValues.Remove(value));
+
+                                    cellsInBlock.Remove(cell);
+                                    emptyCells.Remove(cell);
+                                    _filled.Add(cell);
+                                }
+
+                                var cells = cellsInBlock.Where(c => c.PossibleValues.Count() == 1);
+
+                                foreach (Cell cell in cells)
+                                {
+                                    cell.Value = cell.PossibleValues[0];
+                                    cell.PossibleValues.Clear();
+                                    notKnownValues.Remove(value);
+
+                                    cellsInBlock.Remove(cell);
+                                    emptyCells.Remove(cell);
+                                    _filled.Add(cell);
+
+                                    cellsInBlock.ForEach(c => c.PossibleValues.Remove(cell.Value.Value));
+                                }
+                            }
+
+                            // clear PossibleValues for a block
+                            foreach (var cell in cellsInBlock)
+                                cell.PossibleValues.Clear();
+                        }
+                    }
+                }
+
+                // check if there is no more possibilities for this alghorithm
+                if (emptyCells.Count == initialEmptyCellCount || emptyCells.Count == 0)
+                    proceed = false;
+
+                initialEmptyCellCount = emptyCells.Count;
+            }
+
+            // check if sudoku is solved
+            if (emptyCells.Count == 0) 
+                return;
+
+            // todo: here another more complex attempt
         }
 
         public void Solve()
@@ -91,29 +207,11 @@ namespace SudokuSolver
             throw new NotImplementedException();
         }
 
+        private bool IsPossible(Cell cell, int n)
+        {
+            throw new NotImplementedException();
+        }
 
-        //public void Solve()
-        //{
-        //    // may need to repeat below loop x many times until all cells are filled or all cells left are in 'possoble' state
 
-        //    foreach (var block in _blocks)
-        //    {
-        //        var cells = GetEmptySpaces(block);
-
-        //        if (cells.Count == 0)
-        //            continue;
-
-        //        var missingNumber = GetMissingNumbers(block);
-
-        //        foreach (var cell in cells)
-        //        {
-        //            foreach (var number in missingNumber)
-        //            {
-        //                IsValid()
-        //            }
-        //        }
-
-        //    }
-        //}
     }
 }
